b6c7a913ac5bf660f1cbcdcffc94f2b9d2b87ca6
Virus-777 <63714737+Virus-777@users.noreply.github.com>
2021-07-28 12:11:44 +0530

    akinator added

--- cogs/Games.py
+++ cogs/Games.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import random
+from akinator.async_aki import Akinator
 
 # ____________________________________________________________________________
 
@@ -162,6 +163,81 @@ class Games(commands.Cog):
             await ctx.send("Please enter a position you would like to mark. Correct Syntax: p1/2/3/4/5/6/7/8/9")
         elif isinstance(error, commands.BadArgument):
             await ctx.send("Please make sure to enter an integer.")
+            
+    # __________________________
+    # akinator
+    
+    # _____________________
+    # akinator
+    @commands.command(name="akinator", aliases=["aki"])
+    async def akinator_game(self, ctx):
+        aki = Akinator()
+        first = await ctx.send("Processing...")
+        q = await aki.start_game()
+
+        game_embed = discord.Embed(title=f"{str(ctx.author.name)}'s game of Akinator", description=q,
+                                   url=r"https://en.akinator.com/", color=discord.Color.blurple())
+        game_embed.set_footer(text=f"Wait for the bot to add reactions before you give your response.")
+
+        option_map = {'✅': 'y', '❌': 'n', '🤷‍♂️': 'p', '😕': 'pn', '⁉️': 'i'}
+        """You can pick any emojis for the responses, I just chose what seemed to make sense.
+          '✅' -> YES, '❌'-> NO, '🤷‍♂️'-> PROBABLY YES, '😕'-> PROBABLY NO, '⁉️'->IDK, '😔'-> force end game, '◀️'-> previous question"""
+
+        def option_check(reaction, user):  # a check function which takes the user's response
+            return user == ctx.author and reaction.emoji in ['◀️', '✅', '❌', '🤷‍♂️', '😕', '⁉️', '😔']
+
+        count = 0
+        while aki.progression <= 80:  # this is aki's certainty level on an answer, per say. 80 seems to be a good number.
+            if count == 0:
+                await first.delete()  # deleting the message which said "Processing.."
+                count += 1
+
+            game_message = await ctx.send(embed=game_embed)
+
+            for emoji in ['◀️', '✅', '❌', '🤷‍♂️', '😕', '⁉️', '😔']:
+                await game_message.add_reaction(emoji)
+
+            option, _ = await self.client.wait_for('reaction_add', check=option_check)  # taking user's response
+            if option.emoji == '😔':  # there might be a better way to be doing this, but this seemed the simplest.
+                return await ctx.send("Game ended.")
+            async with ctx.channel.typing():
+                if option.emoji == '◀️':  # to go back to previous question
+                    try:
+                        q = await aki.back()
+                    except:  # excepting trying-to-go-beyond-first-question error
+                        pass
+                    # editing embed for next question
+                    game_embed = discord.Embed(title=f"{str(ctx.author.nick)}'s game of Akinator", description=q,
+                                               url=r"https://en.akinator.com/", color=discord.Color.blurple())
+                    continue
+                else:
+                    q = await aki.answer(option_map[option.emoji])
+                    # editing embed for next question
+                    game_embed = discord.Embed(title=f"{str(ctx.author.nick)}'s game of Akinator", description=q,
+                                               url=r"https://en.akinator.com/", color=discord.Color.blurple())
+                    continue
+
+        await aki.win()
+
+        result_embed = discord.Embed(title="My guess....", colour=discord.Color.dark_blue())
+        result_embed.add_field(name=f"My first guess is **{aki.first_guess['name']}**",
+                               value=aki.first_guess['description'], inline=False)
+        result_embed.set_footer(text="Was I right? Add the reaction accordingly.")
+        result_embed.set_image(url=aki.first_guess['absolute_picture_path'])
+        result_message = await ctx.send(embed=result_embed)
+        for emoji in ['✅', '❌']:
+            await result_message.add_reaction(emoji)
+
+        option, _ = await self.client.wait_for('reaction_add', check=option_check, timeout=15)
+        if option.emoji == '✅':
+            final_embed = discord.Embed(title="I'm a genius", color=discord.Color.green())
+        elif option.emoji == '❌':
+            final_embed = discord.Embed(title="Oof", description="Maybe try again?", color=discord.Color.red())
+            # this does not restart/continue a game from where it was left off, but you can program that in if you like.
+
+            return await ctx.send(embed=final_embed)
+
+
 
 # ______________________________________________________________________
 
